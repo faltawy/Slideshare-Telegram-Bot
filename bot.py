f47a8115493dc85851379746d656861aab0c6772
unknown <ah7255703@gmail.com>
2021-12-01 23:12:52 +0200

    The First Commit

---
+++ bot.py
@@ -0,0 +1,76 @@
+import traceback
+import logging
+import os
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from telegram import Update
+from wrapper import SlideShare, regex
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+token = config.get('token')
+developer_id = config.get('developer_id')
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+def error_handler(update: object, context: CallbackContext) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+
+    logger.error(msg="Exception while handling an update:",
+                 exc_info=context.error)
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__)
+    tb_string = ''.join(tb_list)
+
+# ________________________________________________________
+# ____Bot Functions_______________________________________
+
+
+def start(update: Update, context: CallbackContext):
+    photo = open('photos//slideshare.jpg', 'rb')
+    update.message.reply_photo(
+        photo, caption='ابعت اللينك يا برو واستنى العظمة')
+    update.message.reply_text("""Make Sure That The link in this form 
+    https://www.slideshare.net/victorhernandez9/mobile-me-programming-for-wearables""")
+
+
+def download_slides(update: Update, context: CallbackContext):
+    slideshare = SlideShare()
+    message_link = update.message.text
+    validated_link = slideshare.valid_link(message_link.strip())
+    if validated_link:
+        slides_data = slideshare.slides(message_link)
+        photos = slides_data.get('slides')
+        update.message.reply_text(f"""
+
+        Title : {slides_data.get('title')}
+
+        instructor : {slides_data.get('author')}
+
+        Slides:{slides_data.get('count')}
+
+        *** Due To Server Limitations The Slides Will Be Sent as Photos ***
+        """)
+        for i, photo in enumerate(photos):
+            update.message.reply_photo(
+                photo, caption=f'{slides_data.get("title")} - {i}')
+    else:
+        update.message.reply_text('اللينك دا مش شغال')
+# ________________________________________________________
+
+
+def main() -> None:
+    """Start the bot."""
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_error_handler(error_handler)
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(
+        Filters.regex(regex), download_slides))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ wrapper.py
@@ -0,0 +1,40 @@
+from requests import Session
+from bs4 import BeautifulSoup
+import re
+from errors import LinkNotValid
+
+
+regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]slideshare+)\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"
+
+
+class SlideShare:
+    def __init__(self):
+        self.__main_domain = 'https://www.slideshare.net/'
+
+    def valid_link(self, link: str) -> bool:
+        compiled_regex = re.compile(regex)
+        if not isinstance(link, str):
+            raise LinkNotValid('The Link Must Be str. not int. typo')
+        else:
+            if re.search(compiled_regex, link):
+                return True
+            else:
+                return False
+
+    def slides(self, link: str):
+        print(link)
+        session = Session()
+        s = session.get(link).content
+        soup = BeautifulSoup(s, features='html.parser')
+        title = soup.title.text
+        author = soup.find('a', class_='j-author-name').find('span',
+                                                             {'itemprop': 'name'}).text.strip()
+        slides_imgs = soup.find(
+            'div', {'id': 'slide_container'}).find_all('img')
+        slides_list = list()
+        for slide_img in slides_imgs:
+            src = slide_img.get('src')
+            if len(src) <= 0:
+                src = slide_img.get('data-original')
+            slides_list.append(src)
+        return {'author': author, 'title': title, 'count': len(slides_list), 'slides': slides_list}
