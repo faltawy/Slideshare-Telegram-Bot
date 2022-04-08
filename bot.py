4044064767175621b18928c9ad99e25ab4d62f7e
Ahmed Hassan <76843311+ah7255703@users.noreply.github.com>
2022-04-08 15:32:31 +0200

    s

--- bot.py
+++ bot.py
@@ -1,137 +1,35 @@
-import json
-import html
-__version__ = '0.0.1'
-__author__ = 'Ahmed Hassan'
-import traceback
+import asyncio
+from asyncio.log import logger
 import logging
-import img2pdf
 import os
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
-from telegram import Update, ParseMode
-from wrapper import SlideShare, regex
-from dotenv import dotenv_values
-from requests import get
-config = dotenv_values(".env")
-token = config.get('token', os.environ.get('token'))
-developer_id = config.get('developer_id', os.environ.get('developer_id'))
 
+import dotenv
+from telebot.async_telebot import AsyncTeleBot
+from telebot.types import Message
+dotenv.load_dotenv()
+from wrapper import SlideShare,regex
+log = logging.getLogger(__name__)
 BASE_DIR = os.path.dirname(__file__)
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(filename=os.path.join(BASE_DIR,'log.txt'),level=logging.INFO)
+
+TOKEN = str(os.getenv('token'))
+bot = AsyncTeleBot(TOKEN)
+
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, """\
+        Hi there, I am SlideShare downloader Bot.
+            """)
 
 
-def error_handler(update: object, context: CallbackContext) -> None:
-    """Log the error and send a telegram message to notify the developer."""
-
-    logger.error(msg="Exception while handling an update:",
-                 exc_info=context.error)
-
-    tb_list = traceback.format_exception(
-        None, context.error, context.error.__traceback__)
-    tb_string = ''.join(tb_list)
-
-    update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    message = (
-        f'An exception was raised while handling an update\n'
-        f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
-        '</pre>\n\n'
-        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
-        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
-        f'<pre>{html.escape(tb_string)}</pre>'
-    )
-
-    context.bot.send_message(chat_id=developer_id,
-                             text=message, parse_mode=ParseMode.HTML)
-
-# ________________________________________________________
-# ____Bot Functions_______________________________________
-
-
-def start(update: Update, context: CallbackContext):
-    slide_photo = open('photos/slideshare.jpg', 'rb')
-    update.message.reply_photo(
-        slide_photo, caption='ابعت اللينك يا برو واستنى العظمة')
-    update.message.reply_text("""
-    Make Sure That The link in this form 
-    https://www.slideshare.net/******/****
-    For more information use help command /help
-    """)
-
-
-def download_slides(update: Update, context: CallbackContext):
-    meme_photo = open('photos/meme.jpg', 'rb')
-    slideshare = SlideShare()
-    message = update.message
-    message_link = message.text
-    validated_link = slideshare.valid_link(message_link.strip())
-    # User data
-    user_data = update.message.from_user
-    if validated_link:
-
-        slides_data = slideshare.slides(message_link)
-        if slides_data is not None:
-            photos = slides_data.get('slides')
-
-            message.reply_text(f"""
-        Title : <strong> {slides_data.get('title')}</strong>
-
-        instructor : <strong> {slides_data.get('author')}</strong>
-
-        Slides: <strong> {slides_data.get('count')}</strong> """, parse_mode=ParseMode.HTML)
-
-            # for i, photo in enumerate(photos):
-            #     update.message.reply_photo(
-            #         photo, caption=f'{slides_data.get("title")} - {i+1}')
-
-            photos_data = [get(_).content for _ in photos]
-            pdf_data = img2pdf.convert(photos_data)
-            update.message.reply_document(pdf_data, caption=slides_data.get(
-                'title'), filename=f'{slides_data.get("title")}.pdf')
-
-            notification_msg = (
-                f'User: {user_data.first_name}'
-                f'\nDownloaded This Slide:\n <strong>{slides_data.get("title")}</strong> Successfully'
-                f'\nLink : {message_link}')
-            context.bot.send_message(
-                chat_id=developer_id, text=notification_msg, parse_mode=ParseMode.HTML)
-
-        else:
-            message.reply_photo(meme_photo, caption='اللينك دا مش شغال')
-
-    else:
-
-        message.reply_photo(meme_photo, caption='اللينك دا مش شغال')
-# ________________________________________________________
-
-
-def help(update: Update, context: CallbackContext):
-    user_data = update.message.from_user
-    help_msg = (
-        f'Welcome {user_data.first_name}'
-        '''- Commands -
-        /start - To Start The Bot
-        /help  - To Display This Message\n'''
-        f'- How To Use The Bot -\n'
-        'Send Valid Link Of The Desired Slide from SlideShare'
-
-        'The Bot Will Send You The pdf file'
-    )
-    update.message.reply_text(help_msg, parse_mode=ParseMode.HTML)
-
-
-def main() -> None:
-    """Start the bot."""
-    updater = Updater(token)
-    dispatcher = updater.dispatcher
-    dispatcher.add_error_handler(error_handler)
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('help', help))
-    dispatcher.add_handler(MessageHandler(
-        Filters.regex(regex), download_slides))
-    updater.start_polling()
-    updater.idle()
-
+@bot.message_handler(regexp=regex)
+async def handle_msg(msg:Message):
+    print(msg.text)
+    sh = SlideShare()
+    d  = await sh.slides(msg.text)  # type: ignore
+    print(d)
 
 if __name__ == '__main__':
-    main()
+    logger.info('[*] bot started')
+    asyncio.run(bot.polling())
+    logger.warning('[*] bot stopped')
--- wrapper.py
+++ wrapper.py
@@ -1,47 +1,54 @@
-from requests import Session
+from typing import List
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 import re
-from errors import LinkNotValid
+from dataclasses import dataclass
 
-
+@dataclass
+class Slide:
+    index:int
+    url:str
 regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]slideshare+)\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"
 
+async def valid_link(link: str) -> bool:
+    compiled_regex = re.compile(regex)
+    if re.search(compiled_regex, link):
+        return True
+    return False
+
+@dataclass
+class Slides:
+    author:str
+    title:str
+    slides:List[Slide]
+    @property
+    def count(self):
+        return len(self.slides)
 
 class SlideShare:
     def __init__(self):
         self.__main_domain = 'https://www.slideshare.net/'
+        self.session =ClientSession(raise_for_status=True)
 
-    def valid_link(self, link: str) -> bool:
-        compiled_regex = re.compile(regex)
+    async def __soup(self,url:str)-> BeautifulSoup:
+        async with self.session.get(url) as resp:
+            assert resp.ok
+        soup = BeautifulSoup(await resp.text('utf-8'),'html.parser')
+        return soup
+    
 
-        if not isinstance(link, str):
-            raise LinkNotValid('The Link Must Be str. not int. typo')
-        else:
-            if re.search(compiled_regex, link):
-                return True
-            else:
-                return False
-
-    def slides(self, link: str):
-        session = Session()
-        s = session.get(link)
-        if s.ok:
-            soup = BeautifulSoup(s.content, features='html.parser')
-            title = soup.title.text
-            try:
-                author = soup.find('a', class_='j-author-name').find('span',
-                                                                     {'itemprop': 'name'}).text.strip()
-                slides_imgs = soup.find(
-                    'div', {'id': 'slide_container'}).find_all('img')
-            except:
-                return None
-            slides_list = list()
-            for slide_img in slides_imgs:
-                src = slide_img.get('src')
-                if len(src) <= 0:
-                    src = slide_img.get('data-original')
-                slides_list.append(src)
-            return {'author': author, 'title': title, 'count': len(slides_list), 'slides': slides_list}
-        else:
-            return None
-# SlideShare Search Soon https://www.slideshare.net/search/slideshow?q={Search Term}&page={Pages}
+    async def slides(self, url: str):
+        soup =await self.__soup(url)  # type: ignore
+        title = str(soup.title.text)
+        author = soup.find('div', class_='author-name').find('span',{'itemprop': 'name'}).text.strip()  # type: ignore
+        slides_mcp = soup.find('div', {'id': 'slide-container'}).find_all('img')  # type: ignore
+        slides_list = []
+        for slide_img in slides_mcp:
+            print('fuck')
+            src = slide_img.get('src')
+            if len(src) <= 0:
+                src = slide_img.get('data-original')
+            index = int(slide_img.get('data-index'))
+            slides_list.append(Slide(index=index,url=src))
+        print('fuckkkkkkkkkkk')
+        return Slides(author=author,title=title,slides=slides_list)
